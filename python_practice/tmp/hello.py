d7835cfa0f38b128ea65f90b33d18e9946e554ea
yehaote <nero@neros-MacBook-Pro.local>
2013-10-10 14:01:56 +0800

    python practice

---
+++ python_practice/tmp/hello.py
@@ -0,0 +1,1 @@
+print('Hello, World!') # note that print is a function
---
+++ python_practice/tmp/str_format.py
@@ -0,0 +1,8 @@
+age = 20
+name = 'Swaroop'
+
+print('{0} was {1} years old when he wrote this book'.format(age, name))
+print('Why is {0} playing with that python?'.format(name))
+#The same as following
+print('{} was {} years old when he wrote this book'.format(age, name))
+print('Why is {} playing with that python?'.format(name))
